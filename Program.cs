21dc9dee2a1b770ca4ab143ca9b948e09d760ad3
Deeksha Prabhakar <deekiyengar@gmail.com>
2017-01-21 13:55:58 -0800

    Initial commit
    Strings - Words bucket

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using AlgsDS.Strings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgsDS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(WordsBucket.reverseWords("   fwbpudnbrozzifml osdt  ulc jsx kxorifrhubk ouhsuhf  sswz qfho dqmy  sn myq igjgip iwfcqq"));
+            Console.WriteLine(WordsBucket.lengthOfLastWord("Hello world  ").ToString());            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Strings/WordsBucket.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgsDS.Strings
+{
+    public class WordsBucket
+    {
+         public static int lengthOfLastWord(String a)
+        {
+            int count = 0;
+            bool lastWordStarted = false;
+
+            int i = a.Length - 1;
+            while(i>=0)
+            {
+                if(a[i].ToString() != " ")
+                {
+                    count++;
+                    if (!lastWordStarted)
+                    {
+                        lastWordStarted = true;
+                    }
+                }
+                if (a[i].ToString() == " " && lastWordStarted)
+                {
+                    break;
+                }
+                i--;
+            }
+
+
+            return count;
+        }
+
+         public static String reverseWords(String a)
+         {
+             String[] words = a.Split(' ');
+             StringBuilder sb = new StringBuilder();
+
+             for (int i = words.Length - 1; i >= 0; i--)
+             {
+                 if (words[i].Trim() != "")
+                 {
+                     if (i == 0)
+                     {
+                         sb.Append(words[i]);
+                     }
+                     else
+                     {
+                         sb.Append(words[i] + " ");
+                     }
+                 }
+             }
+
+             if (sb.Length > 1)
+             {
+                 if (sb[sb.Length - 1].ToString().Trim().Length == 0)
+                 {
+                     sb.Remove(sb.Length - 1, 1);
+                 }
+             }
+
+             return sb.ToString();
+         }
+    }
+
+    /*
+        ****Length of Last Word*****
+     
+
+        Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+
+        If the last word does not exist, return 0.
+
+        Note: A word is defined as a character sequence consists of non-space characters only.
+
+        Example:
+
+        Given s = "Hello World",
+
+        return 5 as length("World") = 5.
+
+        Please make sure you try to solve this problem without using library functions. Make sure you only traverse the string once.
+     
+     
+        ***Reverse the String***
+    
+
+        Given an input string, reverse the string word by word.
+
+        Example:
+
+        Given s = "the sky is blue",
+
+        return "blue is sky the".
+
+            "A sequence of non-space characters constitutes a word.
+            Your reversed string should not contain leading or trailing spaces, even if it is present in the input string.
+            If there are multiple spaces between words, reduce them to a single space in the reversed string."
+
+     */
+}
