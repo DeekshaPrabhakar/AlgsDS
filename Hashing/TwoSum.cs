2e52dbde943d0a867c1d0f1b9da1f85ff5b632fd
Deeksha Prabhakar <deekiyengar@gmail.com>
2017-02-01 23:37:36 -0800

    Hashing: 2Sum fixed

--- Hashing/TwoSum.cs
+++ Hashing/TwoSum.cs
@@ -9,7 +9,7 @@ namespace AlgsDS.Hashing
 {
     public class TwoSum
     {
-        public class Node : IEquatable<Node>
+        public class Node : IComparable<Node>
         {
             public int index1;
             public int index2;
@@ -20,53 +20,75 @@ namespace AlgsDS.Hashing
                 this.index2 = ind2;
             }
 
-            public bool Equals(Node other)
+            public int CompareTo(Node other)
             {
                 if (this.index2 < other.index2)
-                    return false;
+                    return -1;
                 if (this.index2 > other.index2)
-                    return true;
-
-                return int.Equals(this.index1, other.index1);
+                    return 1;
+                return this.index1.CompareTo(other.index1);
             }
         }
 
-        public static List<int> twoSumOne(List<int> A, int B)
-        {//LOST COMPLETELY
-            Dictionary<int, int> hashMap = new Dictionary<int, int>();
+        public static List<int> twoSum(List<int> A, int B)
+        {
+            Hashtable hashMap = new Hashtable();
             List<int> resultList = new List<int>();
-            List<Node> node = new List<Node>();
+            List<Node> resultIndexesList = new List<Node>();
 
             int sum = B;
             int diff;
             int size = A.Count;
             int num;
             int index;
-           
+            
+            //go through array from last
             for (int i = size - 1; i >= 0; i--)
             {
                 num = A[i];
                 diff = sum - num;
 
+                //if difference found for current number,store current index and difference index
                 if (hashMap.ContainsKey(diff))
                 {
-                    index = hashMap[diff];
-                    node.Add(new Node(i + 1, index + 1));
+                    index = int.Parse(hashMap[diff].ToString());  
+                    resultIndexesList.Add(new Node(i + 1, index + 1));
                 }
+
+                //store input characters and index in hashtable
                 if (!hashMap.ContainsKey(num))
                 {
-                    hashMap.Add(num, i);
+                    hashMap.Add(num, i);//1,1
+                }
+                else
+                {//update index if number founnd 
+                    hashMap[num] = i;//1,0
                 }
             }
 
-            if (node.Count > 0)
+            if (resultIndexesList.Count > 0)
             {
-                node.Sort();
-                resultList.Add(node[0].index1);
-                resultList.Add(node[0].index2);
+                resultIndexesList.Sort();//sort based on index1 < index2 and for for same index2 minimum index1
+                resultList.Add(resultIndexesList[0].index1);
+                resultList.Add(resultIndexesList[0].index2);
             }
 
             return resultList;
         }       
     }
+    /*
+     * ****************** 2 Sum ********************
+     
+        Given an array of integers, find two numbers such that they add up to a specific target number.
+
+        The function twoSum should return indices of the two numbers such that they add up to the target, where index1 < index2. Please note that your returned answers (both index1 and index2 ) are not zero-based.
+        Put both these numbers in order in an array and return the array from your function ( Looking at the function signature will make things clearer ). Note that, if no pair exists, return empty list.
+
+        If multiple solutions exist, output the one where index2 is minimum. If there are multiple solutions with the minimum index2, choose the one with minimum index1 out of them.
+
+        Input: [2, 7, 11, 15], target=9
+        Output: index1 = 1, index2 = 2
+
+
+     */
 }
--- Program.cs
+++ Program.cs
@@ -234,7 +234,7 @@ namespace AlgsDS
             D.next = new ListNode(6);
             D.next.next = new ListNode(4);
 
-            DisplayLinkedList(AddTwoNumbersAsLists.addTwoNumbers(C, D)); 
+            DisplayLinkedList(AddTwoNumbersAsLists.addTwoNumbers(C, D));
             #endregion
 
             #region WeekTwoAfterSession
@@ -283,10 +283,29 @@ namespace AlgsDS
 
             //Console.WriteLine(LongestSubstring.lengthOfLongestSubstring("Wnb9z9dMc7E8v1RTUaZPoDNIAXRlzkqLaa97KMWLzbitaCkRpiE4J4hJWhRcGnC8H6mwasgDfZ76VKdXhvEYmYrZY4Cfmf4HoSlchYWFEb1xllGKyEEmZOLPh1V6RuM7Mxd7xK72aNrWS4MEaUmgEn7L4rW3o14Nq9l2EN4HH6uJWljI8a5irvuODHY7A7ku4PJY2anSWnfJJE1w8p12Ks3oZRxAF3atqGBlzVQ0gltOwYmeynttUmQ4QBDLDrS4zn4VRZLosOITo4JlIqPD6t4NjhHThOjJxpMp9fICkrgJeGiDAwsb8a3I7Txz5BBKV9bEfMsKNhCuY3W0ZHqY0MhBfz1CbYCzwZZdM4p65ppP9s5QJcfjadmMMi26JKz0TVVwvNA8LP5Vi1QsxId4SI19jfcUH97wmZu0pbw1zFtyJ8GAp5yjjQTzFIboC1iRzklnOJzJld9TMaxqvBNBJKIyDjWrdfLOY8FGMOcPhfJ97Dph35zfxYyUf4DIqFi94lm9J0skYqGz9JT0kiAABQZDazZcNi80dSSdveSl6h3dJjHmlK8qHIlDsqFd5FMhlEirax8WA0v3NDPT8vPhwKpxcnVeu14Gcxr3h1wAXXV0y7Xy9qqB2NQ5HQLJ7cyXAckEYHsLCPSy28xcdNJatx1KLWohOQado4WywJbGvsFR17rKmvOPABweXnFD3odrbSMD4Na4nuBBswvMmFRTUOcf7jZi4z5JnJqXz6hitaPnaEtjoSEBq82a52nvqYy7hhldBoxen2et2OMadVEHeTYLL7GLsIhTP6UizHIuzcJMljo4lFgW5AyrfUlIBPAlhwaSiJtTvcbVZynDSM6RO1PqFKWKg2MHIgNhjuzENg2oFCfW7z5KJvEL9qWqKzZNc0o3BMRjS04NCHFvhtsteQoQRgz84XZBHBJRdekCdcVVXu9c01gYRAz7oIAxN3zKZb64EFKssfQ4HW971jv3H7x5E9dAszA0HrKTONyZDGYtHWt4QLhNsIs8mo4AIN7ecFKewyvGECAnaJpDn1MTTS4yTgZnm6N6qnmfjVt6ZU51F9BxH0jVG0kovTGSjTUkmb1mRTLQE5mTlVHcEz3yBOh4WiFFJjKJdi1HBIBaDL4r45HzaBvmYJPlWIomkqKEmQ4rLAbYG7C5rFfpMu8rHvjU7hP0JVvteGtaGn7mqeKsn7CgrJX1tb8t0ldaS3iUy8SEKAo5IZHNKOfEaij3nI4oRVzeVOZsH91pMsA4jRYgEohubPW8ciXwVrFi1qEWjvB8gfalyP60n1fHyjsiLW0T5uY1JzQWHKCbLVh7QFoJFAEV0L516XmzIo556yRH1vhPnceOCjebqgsmO78AQ8Ir2d4pHFFHAGB9lESn3OtJye1Lcyq9D6X93UakA3JKVKEt6JZDLVBMp4msOefkPKSw59Uix9d9kOQm8WCepJTangdNSOKaxblZDNJ5eHvEroYacBhd9UdafEitdF3nfStF7AhkSfQVC61YWWkKTNdx96OoJGTnxuqt4oFZNFtO7aMuN3IJAkw3m3kgZFRGyd3D3wweagNL9XlYtvZwejbjpkDOZz33C0jbEWaMEaUPw6BG49XqyQoUwtriguO0yvWyaJqD4ye3o0E46huKYAsdKAq6MLWMxF6tfyPVaoqOGd0eOBHbAF89XXmDd4AIkoFPXkAOW8hln5nXnIWP6RBbfEkPPbxoToMbV").ToString());
 
+            Console.WriteLine("Hashing: TwoSum for [4, 7, -4, 2, 2, 2, 3, -5, -3, 9, -4, 9, -7, 7, -1, 9, 9, 4, 1, -4, -2, 3, -3, -5, 4, -7, 7, 9, -4, 4, -8] with target -3");
+            List<int> listOne = new List<int>(new int[] { 4, 7, -4, 2, 2, 2, 3, -5, -3, 9, -4, 9, -7, 7, -1, 9, 9, 4, 1, -4, -2, 3, -3, -5, 4, -7, 7, 9, -4, 4, -8 });
+            DisplayList(TwoSum.twoSum(listOne, -3));
+            Console.WriteLine("");
+
+            Console.WriteLine("Hashing: TwoSum for [1, 1, 1 ] with target 2");
+            listOne = new List<int>(new int[] { 1, 1, 1 });
+            DisplayList(TwoSum.twoSum(listOne, 2));
+            Console.WriteLine("");
+
+            Console.WriteLine("Hashing: TwoSum for [2, 7, 11, 15] with target 9");
+            listOne = new List<int>(new int[] { 2, 7, 11, 15 });
+            DisplayList(TwoSum.twoSum(listOne, 9));
+            Console.WriteLine("");
+
             //Console.WriteLine(WindowString.minWindow("ADOBECODEBANC", "ABC"));
             //Console.WriteLine(WindowString.minWindow("AAAAAA", "AA"));
             //Console.WriteLine(WindowString.minWindow("0mJdGXwLm9AOZ5xA8u92KDYqGJroQ537hoRXjQoUCMOpDYwxoNjexJGWQJAIxSFF3ZbIe27oFxUTJxtv8mORwpuRZn30MDj3kXRW2Ix3lslj7kwmGZPXAKhBW4q5T2BzsqbL0ZETFqRdxVm8GCGfqshvpWzsRvprUcF9vw3VlftqTRzKRzr4zYB2P6C7lg3I7EhGMPukUj8XGGZTXqPqnCqes", "rsm2ty04PYPEOPYO5")); 
+            
             #endregion
+
+            
+           
 
             #region WeekThreeBeforeSession
             //TreeNode notValidBST = new TreeNode(1);
